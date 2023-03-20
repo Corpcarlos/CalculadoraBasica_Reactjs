998b8228d63ca080cac4ef3e0c5d278e803c2ecc
Carlos Ruiz <caosrupe@gmail.com>
2023-03-20 20:24:12 -0300

    creacion

---
+++ src/components/calContainer/CalContainer.js
@@ -0,0 +1,15 @@
+import Calculator from "../calculator/calculator";
+
+
+function contenedor (){
+    return(
+        <>
+        
+        <div className="espacio">
+            <Calculator />
+        </div>
+        </>
+    )
+}
+
+export default contenedor
---
+++ src/components/calculator/calculator.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-eval */
+import React, { useState } from 'react';
+import './Calculator.css'; // importa el archivo CSS
+
+function Calculator() {
+  const [operand1, setOperand1] = useState('');
+  const [operand2, setOperand2] = useState('');
+  const [operator, setOperator] = useState('');
+
+  function handleOperandClick(operand) {
+    if (operator === '') {
+      setOperand1(operand1 + operand);
+    } else {
+      setOperand2(operand2 + operand);
+    }
+  }
+
+  function handleOperatorClick(op) {
+    if (op === '=') {
+      const result = eval(`${operand1}${operator}${operand2}`);
+      setOperand1(result);
+      setOperand2('');
+      setOperator('');
+    } else {
+      setOperator(op);
+    }
+  }
+
+  return (
+    <div className="calculator">
+      <input type="text" value={operand1 + operator + operand2} disabled />
+      <br />
+      <button className="operand" onClick={() => handleOperandClick('1')}>1</button>
+      <button className="operand" onClick={() => handleOperandClick('2')}>2</button>
+      <button className="operand" onClick={() => handleOperandClick('3')}>3</button>
+      <button className="operator" onClick={() => handleOperatorClick('+')}>+</button>
+      <br />
+      <button className="operand" onClick={() => handleOperandClick('4')}>4</button>
+      <button className="operand" onClick={() => handleOperandClick('5')}>5</button>
+      <button className="operand" onClick={() => handleOperandClick('6')}>6</button>
+      <button className="operator" onClick={() => handleOperatorClick('-')}>-</button>
+      <br />
+      <button className="operand" onClick={() => handleOperandClick('7')}>7</button>
+      <button className="operand" onClick={() => handleOperandClick('8')}>8</button>
+      <button className="operand" onClick={() => handleOperandClick('9')}>9</button>
+      <button className="operator" onClick={() => handleOperatorClick('*')}>*</button>
+      <br />
+      <button className="operand" onClick={() => handleOperandClick('0')}>0</button>
+      <button className="operator" onClick={() => handleOperatorClick('=')}>=</button>
+      <button className="operator" onClick={() => handleOperatorClick('/')}>/</button>
+      <button className="clear" onClick={() => { setOperand1(''); setOperand2(''); setOperator(''); }}>C</button>
+    </div>
+  );
+}
+
+export default Calculator;
